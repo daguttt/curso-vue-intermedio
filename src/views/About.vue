bafb29406ab584c1b5da694c08a6a4de830474af
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-01-10 23:11:20 -0500

    Clase 04: Intro a SFC

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,14 @@
 <template>
   <div class="about">
+    <Logo />
     <h1>This is an about page</h1>
   </div>
 </template>
+<script>
+import Logo from "@/components/Logo";
+export default {
+  components: {
+    Logo,
+  },
+};
+</script>
