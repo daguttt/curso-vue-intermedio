46ece390e22eca940056640d79f0e3ce5eb04a43
Daniel Gutierrez Muñoz <dagutmu667@gmail.com>
2022-01-11 17:59:36 -0500

    Clase 07: Auto-importar BaseComponents

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,13 +2,16 @@
   <div class="about">
     <Logo />
     <h1>This is an about page</h1>
+    <BaseForm />
   </div>
 </template>
 <script>
 import Logo from "@/components/Logo";
+// import BaseForm from "@/components/BaseForm.vue";
 export default {
   components: {
     Logo,
+    // BaseForm,
   },
 };
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home">
     <Logo />
+    <BaseForm />
     <Tasks />
   </div>
 </template>
@@ -9,11 +10,13 @@
 // @ is an alias to /src
 import Logo from "@/components/Logo";
 import Tasks from "@/components/Tasks.vue";
+// import BaseForm from "@/components/BaseForm.vue";
 export default {
   name: "Home",
   components: {
     Logo,
     Tasks,
+    // BaseForm,
   },
 };
 </script>
